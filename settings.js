3c3f7d4f9e00c5b0a4059616aea7a03cf898b062
Leobot1810 <137059498+Leobot1810@users.noreply.github.com>
2023-06-20 13:26:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
-//buy apikey premium 0887435047326
+//buy apikey premium 083876459881
 // Free apikey (silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
@@ -24,10 +24,10 @@ global.sewabotnya = `
 edit sewabot di file settings.js line 24
 `
 // setting 
-global.firtsChat = true //send firts message to new user
+global.firtsChat = false //send firts message to new user
 global.autoread = true // auto read message
 global.antidelete = false // anti delete
-global.antiviewonce = true // anti view once
+global.antiviewonce = false // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call, jika true maka org yg nelpon bot auto di blokir
 global.userRegister = false
@@ -39,33 +39,33 @@ global.auto_badword = false //jika true, bot (admin) otomatis menghapus pesan & 
 global.auto_badword2 = true  //jika true, bot (admin) otomatis menghapus pesan member yang mengirim kata kasar
 global.auto_antiwame = false  //jika true, bot (admin) otomatis menghapus pesan & mengeluarkan member yang mengirim link grup
 global.auto_antiwame2 = true  //jika true, bot (admin) otomatis menghapus pesan member yang mengirim link grup
-global.autowelcome = true //auto welcome msg all group
+global.autowelcome = false //auto welcome msg all group
 global.autodetectpromote = true //auto detect admin all group
 global.autodetectdemote = true //auto detect un admin all group
 global.autogoodbye = true //auto goodbye msg all group
-global.groupOnly = false //bot hanya bisa di gunakan dalam grup jika true
+global.groupOnly = true //bot hanya bisa di gunakan dalam grup jika true
 
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 //global.language = english // use this for English language
 //global.language = spanyol // use this for Spanyol language
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.botname = "Chatbot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ Steven" //nama kalian
+global.myweb = "https://s.id/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/" //bebas asal jan hapus
+global.myweb2 = "https://s.id/" //bebas
+global.email = "u883084@gmail.com" //bebas
+global.region = "Jepang" //bebas
 global.timezone = 'Asia/Makassar' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.badword_list = ["asu", "anjir"] //tambahin sendiri kata² nya, gunakan huruf kecil
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62887435047326"] //ganti agar fitur owner bisa di gunakan
+global.badword_list = ["asu", "anjir", "kontol", "ngentod", "ngentot", "babi", "anj", "tolol", "nigga"] //tambahin sendiri kata² nya, gunakan huruf kecil
+global.premium = ['33757050057'] //premium user
+global.owner = ["33757050057", "62887435047326"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = owner[0] + "@s.whatsapp.net"
 global.ownernomerr = "+"+ownernomer //tidak perlu di edit
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
